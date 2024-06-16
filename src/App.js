ad37e5495afba4919d8547480610eb038d5fa847
Ayman Zaki <ayman.za.ghali@gmail.com>
2024-06-16 23:11:41 +0200

    ok fatto

--- src/App.js
+++ src/App.js
@@ -1,70 +1,69 @@
+import { Button, Container } from 'react-bootstrap';
 import './App.css';
+import MyFooter from './components/MyFooter';
+import MyNav from './components/MyNav';
+import Welcome from './components/Welcome';
+import AllTheBooks from './components/AllTheBooks';
+import fantasy from './books/fantasy.json';
+import history from './books/history.json';
+import horror from './books/horror.json';
+import { useState } from 'react';
+import UserComponent from './components/UserComponent';
+import { ThemeContext, AuthContext } from './modules/Contexts';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NotFound from './components/NotFound';
+import BookDetail from './components/BookDetail';
 
 function App() {
+
+  /* let type = 'fantasy';
+  let setType = (val) => {
+    type = val;
+  } */
+/* 
+  let pippo = useState('fantasy');
+  console.log(pippo);
+
+  let type = pippo[0];
+  let setType = pippo[1]; */
+
+  let [type, setType] = useState('fantasy');
+  const [search, setSearch] = useState('');
+  const handleSearch = (e) => setSearch(e.target.value);
+
+  let [theme, setTheme] = useState('light');
+  let [authUser, setAuthUser] = useState('Mario Rossi');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img
-          src="https://media3.giphy.com/media/13CoXDiaCcCoyk/giphy.gif?cid=ecf05e47mkcj61h3gjm3wjb2p4a067eiutl03vig47jsrksp&rid=giphy.gif&ct=g"
-          className="App-logo"
-          alt="cat shoulder shake"
-        />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn <span style={{ textDecoration: 'line-through' }}>Recat</span>{' '}
-          React
-        </a>
-      </header>
-      <main>
-        <p>
-          You probably meant to run <code>npx create-react-app</code> instead of{' '}
-          <code>npx create-recat-app</code>, but we're all here now and you
-          might as well enjoy some cats while you're at it.
-        </p>
-        <p className="images">
-          <img
-            src="https://media4.giphy.com/media/ICOgUNjpvO0PC/giphy.gif?cid=790b76118fbd3d2f0a35e8ff4036a66280840c2540f58474&rid=giphy.gif&ct=g"
-            alt="cat waving"
-          />
-          <img
-            src="https://media2.giphy.com/media/VbnUQpnihPSIgIXuZv/giphy.gif?cid=790b7611a46365babee79d6ce8d46d72eed5122eee0e6ef0&rid=giphy.gif&ct=g"
-            alt="cat developer"
-          />
-          <img
-            src="https://media3.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif?cid=790b7611e3ce0fc616ae6fb98d342b0b8131d965c75fac1b&rid=giphy.gif&ct=g"
-            alt="cat fast typing"
-          />
-          <img
-            src="https://media3.giphy.com/media/12PA1eI8FBqEBa/giphy.gif?cid=790b7611dfb7917b89f875b566ebedb7dc44135253c05172&rid=giphy.gif&ct=g"
-            alt="cat with chicks"
-          />
-          <img
-            src="https://media1.giphy.com/media/krZUvydC7Qrdu/giphy.gif?cid=790b7611c21df70db966c73cf4817e5bd79d5b4711ddc9f9&rid=giphy.gif&ct=g"
-            alt="cat riding ram"
-          />
-          <img
-            src="https://media3.giphy.com/media/10dU7AN7xsi1I4/giphy.gif?cid=790b7611a97153a9f993c7fc252257c0bb66399df74945ab&rid=giphy.gif&ct=g"
-            alt="cat all for one"
-          />
-          <img
-            src="https://media1.giphy.com/media/17Q92poP1qJwI/giphy.gif?cid=790b7611400844530bf6fe0debc6685cbcd4fff4ce6fd067&rid=giphy.gif&ct=g"
-            alt="cat teddy bear"
-          />
-        </p>
-        <p>
-          If you like tools that save you the pain of managing a build system,
-          check out Nx at{' '}
-          <a href="https://nx.dev/react">https://nx.dev/react</a>.
-        </p>
-      </main>
-    </div>
+    <>
+    <ThemeContext.Provider value={[theme, setTheme]}>
+      <AuthContext.Provider value={[authUser]}>
+        <BrowserRouter>
+
+        <MyNav search={search} handleSearch={handleSearch} /> 
+        <Container className="my-3">
+           <Welcome />
+           <Routes>
+              <Route index element={<AllTheBooks books={fantasy} searchQuery={search} />} />
+              <Route path='/details/:asin' element={<BookDetail />} />
+              <Route path='*' element={<NotFound />} />
+           </Routes>
+          {/*
+          <Button variant="dark" className='m-1' onClick={() => setType('history')}>History</Button>
+          <Button variant="dark" className='m-1' onClick={() => setType('fantasy')}>Fantasy</Button>
+          <Button variant="dark" className='m-1' onClick={() => setType('horror')}>Horror</Button>
+          {type === 'fantasy' && <AllTheBooks books={fantasy} searchQuery={search} />}
+          {type === 'history' && <AllTheBooks books={history} searchQuery={search} /> }
+          {type === 'horror' && <AllTheBooks books={horror} searchQuery={search} /> } */}
+
+          {/* <UserComponent /> */}
+        </Container>
+        <MyFooter />
+
+        </BrowserRouter>
+      </AuthContext.Provider>
+    </ThemeContext.Provider>
+    </>
   );
 }
 
